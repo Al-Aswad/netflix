37819b0d248eecd116c870c765342f5fae7df043
Hajar Aswad <hajaraswadkom@gmail.com>
2022-04-19 16:52:14 +0700

    Reguest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner/Bannder'
 import Header from '../components/Header/Header'
+import request from '../utils/request'
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +16,7 @@ const Home: NextPage = () => {
       <Header></Header>
 
       <main>
+        <Banner></Banner>
           {/* Next Js */}
       </main>
 
@@ -23,3 +26,42 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps = async () => {
+
+  const [
+    netflixOriginals,
+    trandingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+
+  ] = await Promise.all([
+    fetch(request.fecthNetflixOriginals).then(res => res.json()),
+    fetch(request.fetchTranding).then(res => res.json()),
+    fetch(request.fetchTopRated).then(res => res.json()),
+    fetch(request.fecthActionMovies).then(res => res.json()),
+    fetch(request.fetchComedyMovies).then(res => res.json()),
+    fetch(request.fetchHorrorMovies).then(res => res.json()),
+    fetch(request.fetchRomanceMovies).then(res => res.json()),
+    fetch(request.fetchDocumentaries).then(res => res.json()),
+  ]);
+
+  return {
+    props:{
+      netflixOriginals: netflixOriginals.results,
+      trandingNow: trandingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    }
+  }
+
+}
