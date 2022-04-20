cd4cbe66143010dda402337cc510dc17e5568a78
Hajar Aswad <hajaraswadkom@gmail.com>
2022-04-20 13:28:44 +0700

    react-icon

--- components/Banner/Bannder.tsx
+++ components/Banner/Bannder.tsx
@@ -1,7 +1,42 @@
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { BASE_URL } from "../../constants/movie"
+import { Movie } from "../../typings"
+import { FaPlay } from 'react-icons/fa'
 
-function Banner() {
+interface Props{
+  netflixOriginals:Movie[]
+}
+
+function Banner({netflixOriginals}:Props) {
+  
+  const [movie, setMovie] =useState<Movie | null>(null)
+
+  useEffect (()=>{
+    setMovie(
+      netflixOriginals[Math.floor(Math.random()* netflixOriginals.length)]
+    )
+  },[netflixOriginals])
+
+  console.log(movie);
+
   return (
-    <div>Banner</div>
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 md:h-[85vh] lg:justify-end lg:pb-12">
+    <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image src={`${BASE_URL}${movie?.backdrop_path || movie?.poster_path}`} layout='fill' objectFit="cover"/>
+      </div>
+        <h1 className="text-2xl lg:text-7xl md:4xl">{movie?.title|| movie?.name}</h1>
+        <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
+
+        <div>
+          <button className="text-black bg-white bannerButton">
+          <FaPlay className="w-4 h-4 text-black md:h-7 md:w-7" />
+            Play
+          </button>
+          <button className="bannerButton">More Info</button>
+        </div>
+
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,33 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner/Bannder'
 import Header from '../components/Header/Header'
+import { Movie } from '../typings'
 import request from '../utils/request'
 
-const Home: NextPage = () => {
+interface Props{
+  netflixOriginals:Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries
+}:Props) => {
+
+  console.log(netflixOriginals);
+
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -16,7 +40,7 @@ const Home: NextPage = () => {
       <Header></Header>
 
       <main>
-        <Banner></Banner>
+        <Banner netflixOriginals={netflixOriginals}/>
           {/* Next Js */}
       </main>
 
